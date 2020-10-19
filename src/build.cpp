cc662a5aafa4f97a70980b02e8443de6b8a6d8f1
Adrián Gómez Brandón <adriangbrandon@gmail.com>
2020-10-19 10:25:11 +0200

    Build method

---
+++ src/build.cpp
@@ -0,0 +1,49 @@
+/***
+BSD 2-Clause License
+
+Copyright (c) 2018, Adrián
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+
+
+//
+// Created by Adrián on 17/10/2020.
+//
+
+#include <iostream>
+#include <repair_sampling.hpp>
+
+int main(int argc, char** argv) {
+
+    if(argc != 4){
+        std::cout << "Usage: " << argv[0] << " input_file period index_file" << std::endl;
+        return 1;
+    }
+    std::string input_file = argv[1];
+    uint32_t period = atoi(argv[2]);
+    std::string index_file = argv[3];
+    cds::repair_sampling<> m_structure(input_file, period);
+    sdsl::store_to_file(m_structure, index_file);
+
+}
